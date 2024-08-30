0079867639b6a3d2f4e1c661d525386dc3943c3d
hotaru <hotaru.py@gmail.com>
2024-08-30 18:02:12 +0530

    mobile ui changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -66,20 +66,20 @@
 </script>
 
 <div class="flex flex-col h-screen w-screen">
-  <div class="ml-10 mt-10 text-xl flex justify-between items-center">
+  <div class="ml-5 mt-5 text-xl flex justify-between items-center md:ml-10 md:mt-10">
     <a class="hover:text-blue-300" href="https://www.youtube.com/shorts/tzD9OxAHtzU">👨🏻🚽 <b>Skibidi</b>GPT</a>
     <a href="https://github.com/hotaru-hspr/SkibidiGPT">
-      <svg class="w-8 h-8 mr-12 fill-current text-white hover:text-blue-300" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg">
+      <svg class="w-8 h-8 mr-6 fill-current text-white hover:text-blue-300 md:mr-12" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg">
         <path fill-rule="evenodd" clip-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8C0 11.54 2.29 14.53 5.47 15.59C5.87 15.66 6.02 15.42 6.02 15.21C6.02 15.02 6.01 14.39 6.01 13.72C4 14.09 3.48 13.23 3.32 12.78C3.23 12.55 2.84 11.84 2.5 11.65C2.22 11.5 1.82 11.13 2.49 11.12C3.12 11.11 3.57 11.7 3.72 11.94C4.44 13.15 5.59 12.81 6.05 12.6C6.12 12.08 6.33 11.73 6.56 11.53C4.78 11.33 2.92 10.64 2.92 7.58C2.92 6.71 3.23 5.99 3.74 5.43C3.66 5.23 3.38 4.41 3.82 3.31C3.82 3.31 4.49 3.1 6.02 4.13C6.66 3.95 7.34 3.86 8.02 3.86C8.7 3.86 9.38 3.95 10.02 4.13C11.55 3.09 12.22 3.31 12.22 3.31C12.66 4.41 12.38 5.23 12.3 5.43C12.81 5.99 13.12 6.7 13.12 7.58C13.12 10.65 11.25 11.33 9.47 11.53C9.76 11.78 10.01 12.26 10.01 13.01C10.01 14.08 10 14.94 10 15.21C10 15.42 10.15 15.67 10.55 15.59C13.71 14.53 16 11.53 16 8C16 3.58 12.42 0 8 0Z" transform="scale(64)"/>
       </svg>
     </a>
   </div>  
 
-  <div class="relative flex flex-col h-full overflow-y-auto p-4 bg-gradient-to-b from-[#EDF0FF]/5 to-transparent mt-10 ml-10 mr-10 rounded-3xl">
+  <div class="relative flex flex-col h-full overflow-y-auto p-2 bg-gradient-to-b from-[#EDF0FF]/5 to-transparent mt-3 ml-3 mr-3 rounded-3xl md:p-4 md:mt-10 md:ml-10 md:mr-10">
     <div class="flex-1">
       {#each convos as convo}
-        <div class={`mb-4 ${convo.type === 'user' ? 'text-right' : ''}`}>
-          <div class={`flex items-start ${convo.type === 'user' ? 'justify-end' : 'justify-start'}`}>
+        <div class="mb-4 ${convo.type === 'user' ? 'text-right' : ''}">
+          <div class="flex items-start ${convo.type === 'user' ? 'justify-end' : 'justify-start'}">
             {#if convo.type === 'bot'}
               <img src="bot.webp" alt="Bot" class="w-6 h-6 rounded-full mr-2" style="align-self: flex-start;" />
             {/if}
@@ -107,7 +107,7 @@
   </div>
 
   <div>
-    <div class="p-2 mt-10 ml-10 mr-10 mb-5 rounded-xl flex items-center">
+    <div class="p-2 mt-5 ml-5 mr-5 mb-3 rounded-xl flex items-center md:m-10">
       <input 
         class="bg-[#1E3345] flex-1 px-4 py-2 rounded-xl outline-none" 
         placeholder="🚽 Feelin' skibidi? Let's yap!" 
@@ -140,3 +140,4 @@
     color: white;
   }
 </style>
+
