34f0b1c76cdbc06293e5a6716a867b869a4b1750
hotaru <hotaru.py@gmail.com>
2024-08-30 11:25:30 +0530

    UI stuff before working on the main part

---
+++ src/routes/+page.svelte
@@ -0,0 +1,40 @@
+<script>
+
+</script>
+
+<div class="flex flex-col h-screen w-screen">
+  <div class="ml-10 mt-10 text-xl hover:text-blue-300 flex justify-between items-center">
+    <a href="https://www.youtube.com/shorts/tzD9OxAHtzU ">👨🏻🚽 SkibidiGPT</a>
+    <a href="https://github.com/hotaru-hspr/SkibidiGPT"><img src="github.webp" alt="GitHub" class="w-8 h-8 mr-12" /></a>
+  </div>
+  
+  <!-- User's message -->
+  <div class="flex-1 overflow-y-auto p-4 bg-[#1E3345] mt-10 ml-10 mr-10 rounded-3xl">
+    <div class="mb-4 text-right">
+      <p class="text-gray-300 px-4 py-2 bg-[#0A2F47] rounded-xl inline-block">Skibidi</p>
+    </div>
+    <!-- Response from LLM -->
+    <div class="mb-2">
+      <p class="p-2 rounded-lg inline-block">Brr Skibidi Dop Dop Dop Yes Yes</p>
+    </div>
+  </div>
+  <div class="p-2 m-10 rounded-xl flex items-center">
+    <input class="border border-white bg-[#1E3345] flex-1 px-4 py-2 rounded-xl" placeholder="🚽 Feelin' skibidi? Let's yap!"/>
+    <button class="ml-2 rounded-xl p-2 bg-[#0A2234] w-16">Send</button>
+  </div>  
+</div>
+
+
+<style lang="postcss">
+  :global(html, body) {
+    background: linear-gradient(180deg, #002F4A, #000D1C);
+    background-attachment: fixed;
+  }
+
+  :global(body) {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: white;
+  }
+</style>
