63c696e03877ce036763aaaff5d413f436a958ad
Try3D <rsarans186@gmail.com>
2024-09-03 10:44:37 +0530

    Don't block harmful content

--- src/routes/api/gen/+server.ts
+++ src/routes/api/gen/+server.ts
@@ -1,10 +1,30 @@
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+import { HarmBlockThreshold, HarmCategory } from "@google/generative-ai";
 
 const llm = new ChatGoogleGenerativeAI({
   model: "gemini-1.5-pro",
   temperature: 0,
   maxRetries: 2,
-  apiKey: "AIzaSyDu3KfxHZDKh7O8_UT8OKXGhvipSNELWgo",
+  apiKey: "AIzaSyDV1ThoQQV1HxxAdgugQaa-FRy4wk3oJDg",
+  safetySettings: [
+    {
+      category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+      threshold: HarmBlockThreshold.BLOCK_NONE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+      threshold: HarmBlockThreshold.BLOCK_NONE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+      threshold: HarmBlockThreshold.BLOCK_NONE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+      threshold: HarmBlockThreshold.BLOCK_NONE,
+    },
+  ],
+
 });
 
 export const GET = async (request: Request): Promise<Response> => {
